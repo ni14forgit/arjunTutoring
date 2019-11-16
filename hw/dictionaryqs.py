87713f12c6e7e8bde0dfbec0a03a1a17f5808e89
Nishant <ni14@duke.edu>
2019-11-15 19:20:24 -0500

    HW Dictionary Python

--- hw/dictionaryqs.py
+++ hw/dictionaryqs.py
@@ -29,5 +29,32 @@ def Last_Names():
 print(Last_Names())
 
 
+'''November 15th, 2019'''
+
 # Problem 2
-#
+
+# Using a for loop (Hint: range(100)) create a dictionary that holds key-value pairs 1:1, 2:2, 3:3 and so on
+
+def numbers():
+    mydict = {}
+#Code to go here
+    print(mydict)
+    return mydict
+
+# Problem 3
+
+# Given any list of strings as an input, for ex: ["Arjun","Nishant","Amy","Bob"], return a dictionary with key value pairs name -> length of string.
+#For example output would be {"Arjun:5,"Nishant":7,"Amy":3,"Bob":3}
+# HINT: len(stringVariable) returns the length of the string
+
+def names():
+    mydict
+    namesList = ["Austin","Cary","Jerry","Rahul","Abby"]
+#Code goes here
+    print(mydict)
+    return mydict
+
+
+
+
+
