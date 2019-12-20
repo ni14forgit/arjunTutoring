c9d7f3ccce555435a71d74c62194cbfabffcf585
Nishant <ni14@duke.edu>
2019-12-19 20:06:30 -0500

    HW Game

---
+++ hw/games/guessgame.py
@@ -0,0 +1,22 @@
+# Create a game that creates a number and then asks the user to guess that number until they get it
+# The program should reply whether the guess is too low or too high
+
+# Starter Code
+
+import random
+
+guess = random.randint(0, 100)
+print("Hi, I've guessed a number, can you guess it?")
+userGuess = -1
+
+while  # Complete this statement :
+
+# OPTIONAL: think about how to handle user input that is not a number?
+# Look online and try things that you find!
+
+userGuess = int(input())
+
+# Complete the rest of the logic, to write whether too low or too high
+
+if userGuess == guess:
+        # how to exit the game?
