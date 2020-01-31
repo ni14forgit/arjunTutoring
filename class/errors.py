21d4c81c537c2b1f0deb0fd0e83514a7235c7876
Nishant <ni14@duke.edu>
2020-01-30 21:00:12 -0500

    lessons

---
+++ class/errors.py
@@ -0,0 +1,14 @@
+# Runtime Error:
+# U can't divide anything by 0 so we are asking the computer to print an error
+
+# if True:
+#     print("Hey look this program is running!")
+#     x = 1/0
+#     print("Arjun is " + str(x) + "years old")
+
+
+# Compilation
+
+arjun = {}
+arjun["age"] = 9
+ishan["age"] = 7
