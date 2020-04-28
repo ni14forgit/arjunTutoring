a9c22b1f8c1a0bc385634fefdaa26801812f8f16
Nishant <ni14@duke.edu>
2020-04-28 15:10:59 -0400

    arjun react app

--- class/recursion.py
+++ class/recursion.py
@@ -1,19 +1,18 @@
-<<<<<<< HEAD
+
 # Print out the number and call if
-Def answer(sequence, n):
+def answer(sequence, n):
 
     if n < 1:
         return sequence
-    Else:
-
+    else:
     #imagine for now sequence = [0,1]
-    indexOfTheLastElement = len(sequence)-1
-    lastElement = sequence[indexOfTheLastElement]
-    secondToLastElement = sequence[len(sequence)-2]
-    Newelem = lastElement + secondToLastElement
-    sequence.add(newelem)
-    answer(sequence, n-1)
-=======
+        indexOfTheLastElement = len(sequence)-1
+        lastElement = sequence[indexOfTheLastElement]
+        secondToLastElement = sequence[len(sequence)-2]
+        Newelem = lastElement + secondToLastElement
+        sequence.add(newelem)
+        answer(sequence, n-1)
+
 def countDownFrom_N(number):
     # Line 3 is known as the "Base Case"
     if number < 1: 
@@ -40,5 +39,5 @@ Sequence -> 0 1 1 2 3 5 8 13 21 34 55 .... and so forth
 # Make a recursive function that produces the first n elements of the fibonacci sequence. 
 
 
->>>>>>> b98449fd6454a6e0f70f78cee103899b4e3b690a
+
 
