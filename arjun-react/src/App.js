541c7ad1eff0735ac583a42237746d42252eb6ff
Arjun Pagidipati <arjun@anion.com>
2020-05-23 11:59:06 -0400

    Add members

--- arjun-react/src/App.js
+++ arjun-react/src/App.js
@@ -1,29 +1,31 @@
 import React, { useState } from "react";
 import "./App.css";
 import List from "./List";
-
-import basketball from "./basketballreact.png";
-import baseball from "./baseball.png";
-
+import Arjun from "./assets/Me.jpg"
+import Dad from "./assets/Dad.png"
+import Mom from "./assets/Mom.png"
+import Ishan from "./assets/Ishan.png"
 import Member from "./Member";
 
 
-
+// This is the app:
 
 const App = () => {
 
 
   const names = ["Arjun", "Dad", "Mom", "Ishan"];
   const description = ["Arjun likes to play basketball, football and tennis. He thinks school is BORING like most other kids.", "Dad runs Zebpay. He likes cryptocurrency.", "Mom is a doctor. She does the heart.", "Ishan likes math. He also likes science, TED and reading. He's basically a nerd."];
-  
-  const myobj = [{name:"Arjun", description: "Arjun likes basketball"}, {name:"Dad", description:"Dad runs Zebpay"}]
-  return(<div>{myobj.map( detailsobj=> {
+  const images = [Arjun, Dad, Mom, Ishan]
+  // myobj is a list
+  const myobj = [{name:names[0], description:description[0], image:images[0]}, {name:names[1], description:description[1], image:images[1]}, {name:names[2], description:description[2], image:images[2]}, {name:names[3], description:description[3], image:images[3]}]
+  // for index, elem in enumerate(myobj): 
+  //  print(elem) apples, bananas
+  //  print(index) 0, 1,  
 
-    return(
-    <Member name={detailsobj.name} description={detailsobj.description}></Member>
+  return(<div>{myobj.map((obj)=> {
+    return (
+      <Member name={obj.name} description={obj.description} image={obj.image}></Member>
     )
-    
-
   })}
   </div>)
 };
